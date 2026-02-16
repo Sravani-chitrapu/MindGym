c50b7af68a2145230b8004bcd592d3092e129efb
Sravani-chitrapu <2005sravani@gmail.com>
2026-02-16 22:56:01 +0530

    //name

--- dashboard.js
+++ dashboard.js
@@ -1,39 +1,56 @@
-let xp = 150;
-let score = 480;
-let streak = 4;
+const brainCard = document.getElementById("brainCard");
+const speedCard = document.getElementById("speedCard");
+const reactionCard = document.getElementById("reactionCard");
+const mathCard = document.getElementById("mathCard");
 
-function openChallenge() {
-  document.getElementById("challengeModal").style.display = "flex";
-}
+/* Brain Explosion */
+brainCard.addEventListener("click", () => {
+    const img = brainCard.querySelector("img");
+    img.classList.add("explode");
 
-function closeChallenge() {
-  document.getElementById("challengeModal").style.display = "none";
-}
+    setTimeout(()=>{
+        document.body.classList.add("zoomOut");
+        setTimeout(()=>{
+            window.location.href="brain.html";
+        },800);
+    },600);
+});
 
-function submitAnswer(correct) {
-  const res = document.getElementById("result");
+/* Speed Burnout */
+speedCard.addEventListener("click", () => {
+    const smoke = document.createElement("div");
+    smoke.classList.add("smoke");
+    speedCard.appendChild(smoke);
 
-  if (correct) {
-    xp += 20;
-    score += 50;
-    streak += 1;
+    setTimeout(()=>{
+        document.body.classList.add("zoomOut");
+        setTimeout(()=>{
+            window.location.href="speed.html";
+        },800);
+    },700);
+});
 
-    document.getElementById("xp").textContent = xp;
-    document.getElementById("score").textContent = score;
-    document.getElementById("streak").textContent = streak;
+/* Reaction Flash */
+reactionCard.addEventListener("click", () => {
+    document.body.style.background="white";
+    setTimeout(()=>{
+        document.body.classList.add("zoomOut");
+        setTimeout(()=>{
+            window.location.href="reaction.html";
+        },800);
+    },300);
+});
 
-    res.textContent = "✅ Correct! +20 XP";
-    res.style.color = "green";
-  } else {
-    res.textContent = "❌ Wrong! Try again tomorrow";
-    res.style.color = "red";
-  }
-}
+/* Math Spin */
+mathCard.addEventListener("click", () => {
+    const img = mathCard.querySelector("img");
+    img.style.transform="scale(1.6) rotate(360deg)";
+    img.style.transition="0.6s";
 
-function goGames() {
-  window.location.href = "brain-games.html";
-}
-
-function goLeaderboard() {
-  window.location.href = "leaderboard.html";
-}
+    setTimeout(()=>{
+        document.body.classList.add("zoomOut");
+        setTimeout(()=>{
+            window.location.href="math.html";
+        },800);
+    },600);
+});
